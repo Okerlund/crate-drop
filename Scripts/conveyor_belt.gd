f93381651ac940105808b8dce15fd3ba0619d1d9
Okerlund <42656116+Okerlund@users.noreply.github.com>
2025-07-28 11:35:56 -0500

    Added Crate Velocity variable
    Added an exported variable for the crate velocity when it is on the conveyor belt.

--- Scripts/conveyor_belt.gd
+++ Scripts/conveyor_belt.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@export var crate_velocity : float = -100.0
+
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("Crate"):
 		body.gravity_scale = 0.0
-		#body.linear_velocity.x = -100.0
+		body.linear_velocity.x = crate_velocity
