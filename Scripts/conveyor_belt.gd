f940580f495630f1b0f65a6c82e0b6dc79f75e56
Okerlund <42656116+Okerlund@users.noreply.github.com>
2025-07-29 16:01:04 -0500

    Added Input
    - Changed crate_velocity to a positive variable and made it negative in the code.
    - Added input for "drop_crate" to space bar (doesn't do anything yet).

--- Scripts/conveyor_belt.gd
+++ Scripts/conveyor_belt.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
-@export var crate_velocity : float = -100.0
+@export var crate_velocity : float = 100.0
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("Crate"):
 		body.gravity_scale = 0.0
-		body.linear_velocity.x = crate_velocity
+		body.linear_velocity.x = -crate_velocity
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_pressed("drop_crate"):
+		pass
